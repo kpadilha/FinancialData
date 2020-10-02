5880817e3a23c46d6e8f437d41e661c10d01a160
kpadilha <krishna@padilha.com>
2020-10-02 15:14:06 -0300

    Scrape working

--- IBOVESPA_Scraping/ibov.py
+++ IBOVESPA_Scraping/ibov.py
@@ -1,11 +1,32 @@
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'http://bvmf.bmfbovespa.com.br/indices/ResumoCarteiraTeorica.aspx?Indice=IBOV&idioma=pt-br'
-page = requests.get(URL)
+pd.set_option('display.min_rows', 50)
+pd.set_option('display.max_rows', 200)
 
-soup = BeautifulSoup(page.content, 'html.parser')
+def busca_carteira_teorica(indice):
+    url = 'http://bvmf.bmfbovespa.com.br/indices/ResumoCarteiraTeorica.aspx?Indice={}&idioma=pt-br'.format(indice.upper())
+    return pd.read_html(url, decimal=',', thousands='.', index_col='Código')[0][:-1]
 
-results = soup.find(id='ctl00_contentPlaceHolderConteudo_Pnltabela')
+def busca_pagina_teorica(indice):
+    url = 'http://bvmf.bmfbovespa.com.br/indices/ResumoCarteiraTeorica.aspx?Indice={}&idioma=pt-br'.format(indice.upper())
+    page = requests.get(url)
+    return page
 
-print(results.prettify())
+ibov = busca_carteira_teorica('ibov')
+ibov.sort_values('Part. (%)', ascending=False)
+
+ibovpagina = busca_pagina_teorica('ibov')
+
+soup = BeautifulSoup(ibovpagina.content,'html.parser')
+results = soup.find(id='ctl00_contentPlaceHolderConteudo_grdResumoCarteiraTeorica_ctl00_ctl03_ctl00_lblRedutorResultado')
+
+#redutor = results.find_all('span', class_='label')
+
+txt = results.text 
+x = txt.replace(".", "")
+y = x.replace(",", ".")
+z = float(y)
+print (ibov)
+
